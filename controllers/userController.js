c0c88a8f38a403642ab7cd7823a991d5aac79caf
rlawlsgus <jh0623.kim@gmail.com>
2020-02-20 04:27:38 +0900

    Initial Commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,8 +17,17 @@ export const postJoin = (req, res) => {
     }
 }
 
-export const login = (req, res) => res.render("login", {pageTitle: "Login"});
-export const logout = (req, res) => res.render("logout", {pageTitle: "Logout"});
+export const getLogin = (req, res) => {
+    res.render("login", {pageTitle: "Login"});
+}
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home)
+}
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
 export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import {videos} from "../db"
+import routes from "../routes"
 
 export const home = (req, res) => {
     res.render("home", {pageTitle: "Home", videos});
@@ -9,7 +10,18 @@ export const search = (req, res) => {
     res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
 
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const postUpload = (req, res) => {
+    const {
+        body: {
+            file,
+            title,
+            description
+        }
+    } = req;
+    // To Do: Upload and sqve video
+    res.redirect(routes.videoDetail())
+}
 
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 
