655ef45ff4679fed5d9499feb3b93b1a20fba046
rlawlsgus <jh0623.kim@gmail.com>
2020-02-23 02:08:54 +0900

    Initial Commit

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,27 +1,37 @@
-import {videos} from "../db"
-import routes from "../routes"
+import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    res.render("home", {pageTitle: "Home", videos});
-}
-
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({});
+        res.render("home", { pageTitle: "Home", videos });
+    } catch (error) {
+        console.log(error);
+        res.render("home", { pageTitle: "Home", videos: [] });
+    }
+};
 export const search = (req, res) => {
-    const {query: {term: searchingBy}} = req;
-    res.render("search", {pageTitle: "Search", searchingBy, videos});
-}
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTitle: "Search", searchingBy, videos });
+};
 
 export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"});
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
     const {
-        body: {
-            file,
-            title,
-            description
-        }
+        body: { title, description },
+        file: { path }
     } = req;
-    // To Do: Upload and sqve video
-    res.redirect(routes.videoDetail())
-}
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    console.log(newVideo)
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(newVideo.id));
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 
