606c3122792d57005176029487eb6778b289b2c1
rlawlsgus <jh0623.kim@gmail.com>
2020-03-20 17:32:28 +0900

    Initial Commit

--- routes.js
+++ routes.js
@@ -28,6 +28,12 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 const GOOGLE = "/auth/google";
 const GOOGLE_CALLBACK = "/auth/google/callback";
 
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -71,7 +77,10 @@ const routes = {
   githubCallback: GITHUB_CALLBACK,
   google: GOOGLE,
   googleCallback: GOOGLE_CALLBACK,
-  me: ME
+  me: ME,
+  api: API,
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT
 };
 
 export default routes;
